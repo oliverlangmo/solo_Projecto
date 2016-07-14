8571d14cc5600035e7f71e4beea61630cc7a28b8
oliverlangmo <oliver.langmo@gmail.com>
2016-07-14 10:13:19 -0500

    update modal diplaying

--- public/scripts/FE-app.js
+++ public/scripts/FE-app.js
@@ -152,6 +152,26 @@ $scope.deleteExpense = function(){
 });
 $scope.seeExpenses();
 };
+$scope.modalShown = false;
+$scope.toggleModal = function() {
+  $scope.modalShown = !$scope.modalShown;
+};
+$scope.updateExpense = function(){
+  var expenseToUpdate={
+    id: event.target.id,
+    planYear: $scope.pyIn,
+    description: $scope.whatexpenseIn,
+    amount:$scope.expenseAmtIn,
+    date: $scope.expenseDateIn,
+    receipt: $scope.receiptImgIn
+    };
+    console.log(expenseToUpdate);
+    $http({
+     method: 'PUT',
+     url: '/updateExpense',
+     data: expenseToUpdate
+    });
+};
 }]);
 myApp.controller('planYearSetup', ['$scope','$http', function($scope, $http){
   $scope.sendYearInfo = function(){
@@ -195,6 +215,33 @@ myApp.controller('getPlanYearDeetz', ['$scope', '$http', function($scope, $http)
           }, function myError( response ) {
               console.log( response.statusText ) ;
 });
-
- };
+};
 }]);
+myApp.directive('modalDialog', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      show: '='
+    },
+    replace: true, // Replace with the template below
+    transclude: true, // we want to insert custom content inside the directive
+    link: function(scope, element, attrs) {
+      scope.dialogStyle = {};
+      if (attrs.width)
+        scope.dialogStyle.width = attrs.width;
+      if (attrs.height)
+        scope.dialogStyle.height = attrs.height;
+      scope.hideModal = function() {
+        scope.show = false;
+      };
+    },
+    template: "<div class='ng-modal' ng-show='show'><div class='ng-modal-overlay' ng-click='hideModal()'></div><div class='ng-modal-dialog' ng-style='dialogStyle'><div class='ng-modal-close' ng-click='hideModal()'>X</div><div class='ng-modal-dialog-content' ng-transclude></div></div></div>"
+  };
+});
+
+// myApp.controller('MyCtrl', ['$scope', function($scope) {
+//   $scope.modalShown = false;
+//   $scope.toggleModal = function() {
+//     $scope.modalShown = !$scope.modalShown;
+//   };
+// }]);
